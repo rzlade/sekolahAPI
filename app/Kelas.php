42b5eb257bdc243a4534ddd24fa3337e8c2c874a
rzlade <khoerulikhsan.r@gmail.com>
2019-02-14 15:57:00 +0700

    Project Lumen sekolahAPI

---
+++ app/Kelas.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Kelas extends Model
+{
+    public $table = 't_kelas';
+
+    protected $fillable = [
+        'nama_kelas', 'jurusan'
+    ];
+}
+
+?>
